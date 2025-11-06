c82d663eb4c470303b392cdca5b6e69988c22d98
amon4090 <amnafarwa4@gmail.com>
2025-11-06 14:16:39 +0300

    اضافة كود صفحة payment_success_dialog

---
+++ lib/features/books/presentation/widgets/payment_success_dialog.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PaymentSuccessDialog extends StatelessWidget {
+  const PaymentSuccessDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("✅ تمت العملية بنجاح"),
+      content: const Text("تم شراء الكتاب وإضافته إلى سلة المشتريات."),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text("حسنًا"),
+        ),
+      ],
+    );
+  }
+}
